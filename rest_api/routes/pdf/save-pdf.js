b2c05b42e6443ead5296d0fedd40082fd491eb23
Jonathan Neidel <github@jneidel.com>
2019-03-31 06:18:42 +0200

    Add basic pdf generation

--- rest_api/routes/pdf/save-pdf.js
+++ rest_api/routes/pdf/save-pdf.js
@@ -1,10 +1,8 @@
 const fs = require( "mz/fs" );
 const path = require( "path" );
 
-const directory = path.resolve( __dirname, "..", "..", "data", "pdf" );
 
 function savePdf( name, buffer ) {
-  const filePath = path.resolve( directory, `${name}.pdf` );
 
   fs.writeFile( filePath, buffer, { encoding: "base64" } )
 }
